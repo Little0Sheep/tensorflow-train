62da78d9f5f47e911e7563cdd76b1baa3040c416
sunjinping <997318740@qq.com>
2017-06-20 16:07:54 +0800

    add one comment

--- mnist/mnist_with_summaries.py
+++ mnist/mnist_with_summaries.py
@@ -25,11 +25,15 @@ If you modify this file, please update the excerpt in
 how_tos/summaries_and_tensorboard/index.md.
 
 """
+
+#add comment
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+
 
 from tensorflow.examples.tutorials.mnist import input_data
 
